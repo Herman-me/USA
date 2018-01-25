25ae00452ec94ac378a1ff6b6a382609127ef8a5
Mehdi Rahimi <nabirios.mr@gmail.com>
2018-01-25 19:15:19 +0330

    :)

--- st(1).sql
+++ st(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 23, 2018 at 05:33 PM
+-- Generation Time: Jan 25, 2018 at 04:44 PM
 -- Server version: 10.1.22-MariaDB
 -- PHP Version: 7.1.4
 
@@ -47,6 +47,44 @@ CREATE TABLE `ostad` (
   `lessencode` int(3) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
 
+--
+-- Dumping data for table `ostad`
+--
+
+INSERT INTO `ostad` (`id`, `fname`, `personalcode`, `lessencode`) VALUES
+(1, 'علی اصلانی', '245', 0),
+(2, 'علی اصلانی', '245', 0),
+(3, 'علی اصلانی', '245', 0),
+(4, 'علی اصلانی', '245', 0),
+(5, 'علی اصلانی', '245', 0),
+(6, 'علی اصلانی', '245', 0),
+(7, 'علی اصلانی', '245', 0),
+(8, 'علی اصلانی', '245', 0),
+(9, 'محسن موحدی', '123', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `selected_lessen`
+--
+
+CREATE TABLE `selected_lessen` (
+  `id` int(11) NOT NULL,
+  `lessen` int(100) NOT NULL,
+  `for_st` int(11) NOT NULL,
+  `score` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+--
+-- Dumping data for table `selected_lessen`
+--
+
+INSERT INTO `selected_lessen` (`id`, `lessen`, `for_st`, `score`) VALUES
+(1, 4, 5, 0),
+(2, 5, 5, 0),
+(3, 3, 5, 19),
+(4, 4, 5, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -76,6 +114,12 @@ ALTER TABLE `ostad`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `selected_lessen`
+--
+ALTER TABLE `selected_lessen`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `student`
 --
 ALTER TABLE `student`
@@ -94,7 +138,12 @@ ALTER TABLE `lessions`
 -- AUTO_INCREMENT for table `ostad`
 --
 ALTER TABLE `ostad`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `selected_lessen`
+--
+ALTER TABLE `selected_lessen`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `student`
 --
